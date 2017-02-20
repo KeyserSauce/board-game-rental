433538b743e00e8fd1fd8706d0984be6a0ca82a4
Dominik Moehrle <dominik.moehrle@gmail.com>
2017-02-20 17:10:52 +0100

    background pic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,13 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+$("#getstarted").click(function() {
+    $('index.html,body').animate({
+        scrollTop: $("#signup").offset().top},
+        'slow');
+});
+})
+
+
