3f2825894c0adad1738edfb6192c509a653a5d99
Elliott Potter <elliott@shipt.com>
2017-02-20 16:43:46 +0100

    Add description column to games, before_action to games controller, skeleton of show for game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220143438) do
+ActiveRecord::Schema.define(version: 20170220153558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20170220143438) do
     t.string   "name"
     t.integer  "price"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
     t.index ["user_id"], name: "index_games_on_user_id", using: :btree
   end
 
