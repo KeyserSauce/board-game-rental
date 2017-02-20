d896e989b2f827c2ebb48e7cd7739d55ec850597
Soren Jensen <sorenkbj@gmail.com>
2017-02-20 15:51:45 +0100

    added database structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220113129) do
+ActiveRecord::Schema.define(version: 20170220143438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id", using: :btree
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "game_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_rentals_on_game_id", using: :btree
+    t.index ["user_id"], name: "index_rentals_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +52,7 @@ ActiveRecord::Schema.define(version: 20170220113129) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "games", "users"
+  add_foreign_key "rentals", "games"
+  add_foreign_key "rentals", "users"
 end
