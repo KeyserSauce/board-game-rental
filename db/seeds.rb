efdd811e6c7fe5963e4176b17e479c01672e4b3b
Elliott Potter <elliott@shipt.com>
2017-02-24 15:31:00 +0100

    Beautify dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -94,19 +94,19 @@ def get_photo_url(name)
 end
 
 # Create users
-20.times do
-  user                    = User.new
-  user.email              = Faker::Internet.email
-  user.password           = Faker::Internet.password.to_s
-  user.save!
-end
+# 20.times do
+#   user                    = User.new
+#   user.email              = Faker::Internet.email
+#   user.password           = Faker::Internet.password.to_s
+#   user.save!
+# end
 
 # Create games
-games.each do |g|
+games.first(10).each do |g|
   puts games
   game                    = Game.new
   game.name               = g
-  game.user               = User.order("RANDOM()").first
+  game.user               = User.find(216) #User.order("RANDOM()").first
   game.description        = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae libero ac arcu consequat feugiat. Cras justo ipsum, egestas at volutpat in, fermentum sed est. Cras dolor lacus, vestibulum sit amet malesuada in, viverra sit amet sapien. Morbi non augue volutpat, tempor erat et, rhoncus lorem. Aenean efficitur sollicitudin libero sit amet pellentesque. Nunc sollicitudin risus at mauris imperdiet commodo. Nullam imperdiet dictum sem, quis elementum quam. Duis euismod tristique magna scelerisque tempus. Donec ligula diam, varius at eleifend venenatis, venenatis ut nisi. Morbi cursus nulla vitae lacinia iaculis. Aliquam pharetra quis elit nec faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean vitae mauris vitae odio molestie dapibus et eget metus."
   game.category           = ["family", "card", "german", "children", "drinking", "group"].sample
   game.photo_url          = get_photo_url(g)
@@ -119,7 +119,7 @@ end
   rental                  = Rental.new
   rental.start_date       = Date.today
   rental.end_date         = Date.today
-  rental.user_id          = User.order("RANDOM()").first.id
+  rental.user_id          = 216  #User.order("RANDOM()").first.id
   rental.game_id          = Game.order("RANDOM()").first.id
   rental.save!
 end
